4eb62e5b0585ff71f2ff77bc1781f165d2f55a9a
akhilesh rawat <akhileshrawat76@gmail.com>
2022-03-07 14:17:43 +0530

    add backend, error, loading

---
+++ frontend/src/components/MessageBox.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const MessageBox = (props) => {
+  const { variant, children } = props;
+  return <div className={`alert alert-${variant || "info"}`}>{children}</div>;
+};
+
+export default MessageBox;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,44 @@
-import { data } from "../data";
+import axios from "axios";
 import Product from "../components/Product";
+import { useEffect, useState } from "react";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get("/api/products");
+        setLoading(false);
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="row center">
-      {data.products.map((product) => (
-        <Product product={product} key={product._id} />
-      ))}
-    </div>
+    <>
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <div className="row center">
+          {products.map((product) => (
+            <Product product={product} key={product._id} />
+          ))}
+        </div>
+      )}
+    </>
   );
 };
 
