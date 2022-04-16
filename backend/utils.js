e1b3864ddbe967a8665a16dd210e79f92c1679f4
akhilesh rawat <akhileshrawat76@gmail.com>
2022-04-16 13:25:59 +0530

    fix bug from orderRouter

--- backend/utils.js
+++ backend/utils.js
@@ -16,10 +16,10 @@ export const generateToken = (user) => {
 };
 
 export const isAuth = (req, res, next) => {
-  const authorization = req.header.authorization;
+  const authorization = req.headers.authorization;
   if (authorization) {
-    const token = authorization.slice(7, authorization.length); // Bearer xxxxxx
-    jwt.varify(
+    const token = authorization.slice(7); // Bearer xxxxxx
+    jwt.verify(
       token,
       process.env.JWT_SECRET || "somethingsecret",
       (err, decode) => {
